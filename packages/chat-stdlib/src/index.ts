89f298b683b45c2cedc9ab11943bc34fe96c944e
Evan Bremer <evanbb@gmail.com>
2023-10-27 12:57:19 -0500

    update api surface area

--- packages/chat-stdlib/src/index.ts
+++ packages/chat-stdlib/src/index.ts
@@ -72,7 +72,7 @@ ${err.stack || '(no stack trace information)'}`
  */
 export function stringifyError(
   err: unknown,
-  errorDescription?: string,
+  errorDescription: string,
 ) {
   return `${errorDescription ?? '( no error description )'}\n${
     err instanceof Error
